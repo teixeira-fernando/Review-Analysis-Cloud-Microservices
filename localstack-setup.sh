4e3cc659ea8cd7c275b8b01365caf0c7f5438363
fernando <fernandoadt1@gmail.com>
2025-01-12 16:28:16 +0100

    code cleanup in Dockerfiles and docker-compose.yml

--- localstack-setup.sh
+++ localstack-setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 echo "Initializing localstack s3"
 
-awslocal s3 mb s3://review-analysis-bucket
 awslocal s3api create-bucket --bucket review-analysis-bucket
 awslocal sqs create-queue --queue-name review-analysis-queue
-
-# List the contents of the S3 bucket
-awslocal s3 ls s3://review-analysis-bucket
 
 echo "Executed commands to setup localstack s3 and sqs"
