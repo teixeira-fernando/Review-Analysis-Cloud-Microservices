914677889e7b2dfb6c3fd3eaede3e5808ecaf8fc
Fernando Teixeira <fernando.teixeira@verivox.com>
2026-04-14 16:21:48 +0200

    small adjustment to testcontainer-config file

--- frontend-review/e2e/testcontainers-config.js
+++ frontend-review/e2e/testcontainers-config.js
@@ -10,7 +10,7 @@ async function setupContainers() {
       path.dirname(composeFile),
       path.basename(composeFile)
     )
-      .withWaitStrategy('localstack', Wait.forLogMessage('Ready'))
+      .withWaitStrategy('ministack', Wait.forLogMessage('Ready'))
       .withWaitStrategy('review-collector', Wait.forHttp("/actuator/health", 8080).forStatusCode(200))
       .withWaitStrategy('review-analyzer', Wait.forHttp("/actuator/health", 8081).forStatusCode(200))
       .up();
@@ -18,8 +18,8 @@ async function setupContainers() {
   console.log('Containers started successfully.');
 
   //execute custom commands inside LocalStack container
-  await testContainersRuntime.getContainer('localstack').exec(['awslocal', 's3', 'mb', 's3://review-analysis-bucket']);
-  await testContainersRuntime.getContainer('localstack').exec(['awslocal', 'sqs', 'create-queue', '--queue-name', 'review-analysis-queue']);
+  await testContainersRuntime.getContainer('ministack').exec(['awslocal', 's3', 'mb', 's3://review-analysis-bucket']);
+  await testContainersRuntime.getContainer('ministack').exec(['awslocal', 'sqs', 'create-queue', '--queue-name', 'review-analysis-queue']);
 
   return testContainersRuntime;
 }
