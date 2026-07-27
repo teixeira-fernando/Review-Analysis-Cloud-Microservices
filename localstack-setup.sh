5e9e8bef8a84b3d51d2adad7a0421a51d9507a7b
fernando <fernandoadt1@gmail.com>
2026-07-27 20:53:27 +0200

    using same standard to set the same region for all the resources

--- localstack-setup.sh
+++ localstack-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Initializing localstack s3"
 
-awslocal --region eu-central-1 s3api create-bucket --bucket review-analysis-bucket
+awslocal --region eu-central-1 s3api create-bucket --bucket review-analysis-bucket --create-bucket-configuration LocationConstraint=eu-central-1
 awslocal --region eu-central-1 sqs create-queue --queue-name review-analysis-queue
 
 echo "Executed commands to setup localstack s3 and sqs"
